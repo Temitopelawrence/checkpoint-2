462718df24fdf46a0ca061f15e0d6b5366e995eb
Kennylawrence <Kennylawrence259@gmail.com>
2023-11-19 13:21:26 +0100

    first commit

---
+++ openai/index.js
@@ -0,0 +1,99 @@
+// function rem(){
+//     prompt('Enter your age')
+// }
+
+// function gavi(){
+//     confirm('Are you sure you want to continue?')
+//     prompt("what's your name?")
+//     alert('Welcome to our page')
+// }
+
+// function kenny(){
+//     // return 4 + 8
+//     console.log( 4 + 9);
+    
+    
+// }
+
+// const person = {
+//     name: 'Fareed',
+//     age:  35,
+//     married: false,
+//     graduate: null,
+//     landlord: 'yes',
+//     student: true,
+// } 
+
+// console.log(person.landlord);
+
+const colours = ['red', 'white', 'indigo', 'green', 'yellow']
+// colours.pop() removes the last element 
+// console.log(colours);  
+
+// colours.push('black') adds to the last element
+// console.log(colours);
+
+// colours.shift() removes from the first element 
+// console.log(colours);
+
+// colours.unshift('azure') adds to the first Element
+// console.log(colours);
+
+
+
+// An arrray method that can add to the first element of the array
+// An array method that can remove from the first element of the array
+// An array method that can add to the last element of the array
+// An array method that can remove from the last element of the array
+
+// 1)
+// const array = [1, 2, 3]
+// array.unshift(7); (works also)
+// console.log(array);
+
+const properties=[
+    'shoes',
+    'shirts',
+    cars=['Porsche', 'Bugatti', 'Toyota', 'Tesla'],
+    houses =['Penthouse', 'Duplex', 'Bungalow'],
+    'Shares',
+    'cash'
+]
+// console.log(properties.length);
+// let index = properties.indexOf("Shares"); to find index in an array method-1
+// console.log(index);
+
+// console.log(properties[4]);  method-2
+
+// to get values of nested array
+// console.log(properties[2][3]);
+// console.log(properties [3][1]);
+
+// This is an array of Objects [index is in this ]  
+// const players =[
+// {
+//     name: 'Lionel messi',
+//     age: 36,
+//     club: 'Inter Miami',
+//     country: 'Argentina'
+// },
+// {
+//     name: 'Christiano Ronaldo',
+//     age: 38,
+//     club: 'Al Nassr',
+//     country: 'Portugal'
+// },
+// {
+//     name: 'Reece james',
+//     age: 25,
+//     club: 'Chelsea',
+//     country: 'England'
+// },
+// {
+//     name: 'Cole Palmer',
+//     age: 23,
+//     club: 'Chelsea',
+//     country: 'England'
+// }
+// ]
+// console.log(players[3].name, players[1].country);
